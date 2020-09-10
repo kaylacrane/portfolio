aa38f081f866d908bc3750ee98214043bde4ec56
Kayla Jean Crane <kayla.crane@gmail.com>
2020-09-10 14:50:03 +0200

    update text and css on cover page

--- src/components/CoverPage.js
+++ src/components/CoverPage.js
@@ -7,10 +7,18 @@ function CoverPage() {
     <>
       <div className="cover">
         <div className="cover__text">
-          <span className="hand shake-br">👋</span>
-          <h2>Hi there</h2>
-
-          <span>See you then!</span>
+          <span className="hand wobble-hor-bottom">👋</span>
+          <h2 className="scale-in-hor-left">Hi there!</h2>
+          <span>You are staring at the frontpage of my portfolio.</span>
+          <span>
+            <span className="highlight">
+              <small className="decoration fas fa-bahai"></small>Welcome
+              <small className="decoration fas fa-bahai"></small>
+            </span>
+          </span>
+          <span>Please take a look around.</span>
+          <span>I hope you enjoy!</span>
+          <small>-kayla</small>
         </div>
       </div>
     </>
