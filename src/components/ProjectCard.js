7ce1ec2775a6a1bcb95b9b607c3c8b454e9fc6bf
Kayla Jean Crane <kayla.crane@gmail.com>
2020-09-14 11:21:57 +0200

    refactor project card open info function

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,15 +2,22 @@ import React from "react";
 
 function ProjectCard(props) {
   const { name, image, github, url, description, id } = props.project;
+  const { openInfo, stopPropagation, openProjectIDs } = props;
+
   return (
-    <li className="project-card scale-in-top" id={id}>
+    <li className="project-card scale-in-top" id={id} onClick={openInfo}>
       <div
         className="image-container"
         alt={name}
         style={{ backgroundImage: `url(${image})` }}
       ></div>
 
-      <div className="project-info">
+      <div
+        className={`project-info ${
+          openProjectIDs.includes(id) ? "open-info" : ""
+        }`}
+        onClick={stopPropagation}
+      >
         <h3 className="name">{name}</h3>
         <div className="links">
           <a
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
 import { projectsList } from "../data/projectsList";
 
-const createProjectCards = projectsList.map((project, index) => {
-  return <ProjectCard key={index} project={project}></ProjectCard>;
-});
-// function openInfo(event) {
-//   const project = event.currentTarget;
-//   const info = project.querySelector(".project-info");
-//   info.classList.toggle("open-info");
-// }
-// function stopPropagation(event) {
-//   event.stopPropagation();
-// }
 function Projects() {
+  const [openProjectIDs, setOpenProjectIDs] = useState([]);
+
+  function openInfo(event) {
+    const project = parseInt(event.currentTarget.id);
+    if (!openProjectIDs.includes(project)) {
+      setOpenProjectIDs([...openProjectIDs, project]);
+    } else {
+      const newIDs = openProjectIDs.filter((id) => id !== project);
+      setOpenProjectIDs(newIDs);
+    }
+  }
+  function stopPropagation(event) {
+    event.stopPropagation();
+  }
+
+  const createProjectCards = projectsList.map((project, index) => {
+    return (
+      <ProjectCard
+        key={index}
+        project={project}
+        openInfo={openInfo}
+        stopPropagation={stopPropagation}
+        openProjectIDs={openProjectIDs}
+      ></ProjectCard>
+    );
+  });
   return (
     <div className="projects-page">
       <h2 className="title">My Projects</h2>
